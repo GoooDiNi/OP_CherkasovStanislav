ce69401bc142dee36c4a84655e5aa77b30d3a037
GoooDiNi <90209078+GoooDiNi@users.noreply.github.com>
2021-09-19 22:19:00 +0300

    Update OP_Lab1.cpp

--- OP_Lab1.cpp
+++ OP_Lab1.cpp
@@ -6,36 +6,36 @@ int main(void)
 {
 	setlocale(LC_ALL, "Ukrainian");
 	
-	//вводимо та задаємо змінні
+	//input
 	float a, b, c, P, p, S;
 	cout << "a="; cin >> a;
 	cout << "b="; cin >> b;
 	cout << "c="; cin >> c;
 
-	//якщо заданий трикутник існує:
+	//if the triangle exists
 	if (a < b + c and b < a + c and c < a + b)
 	{
-		//обраховуємо площу трикутника за формулою Герона
+		//calculate the area
 		P = a + b + c;
 		p = P / 2;
 		S = sqrt(p * (p - a) * (p - b) * (p - c));
 
-		//обраховуэмо та вивиодимо бісектриси трикутника
-		cout << "\nБiсектриси трикутника:\n";
+		//calculate and output the bisectors
+		cout << "\The bisectors:\n";
 		printf("l1=%5.3f\n", sqrt(a * b * P * (a + b - c)) / (a + b));
 		printf("l2=%5.3f\n", sqrt(b * c * P * (b + c - a)) / (b + c));
 		printf("l3=%5.3f\n", sqrt(c * a * P * (c + a - b)) / (c + a));
 
-		//обраховуємо та виводимо радіуси вписанного та описанного кіл
-		cout << "\nРадiуси вп. та оп. кiл:\n";
+		//calculate and output the radii
+		cout << "\The radii:\n";
 		printf("R=%5.3f\n", (a * b * c) / (4 * S));
 		printf("r=%5.3f\n", (2 * S) / P);
 	}
 
-	//якщо заданий трикутник не існує:
+	//if the triangle doesn't exist
 	else
 	{
-		cout << "\nШановний(а), ви вiдвiдуєте не той навчальний заклад";
+		cout << "The triangle doesn't exist";
 		return 0;
 	}
 }
