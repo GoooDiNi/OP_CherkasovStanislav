72933e295e7b5f9a9ba9ab2772a114d45d357f82
GoooDiNi <90209078+GoooDiNi@users.noreply.github.com>
2021-09-27 18:27:38 +0300

    Update OP_Lab1.cpp

--- OP_Lab1.cpp
+++ OP_Lab1.cpp
@@ -1,39 +1,35 @@
-#include "stdlib.h"
 #include "math.h"
 #include <iostream>
 using namespace std;
-int main(void)
+
+int main()
 {
-	//input
-	float a, b, c, P, p, S;
+	float a, b, c, P, p, S, l1, l2, l3, R, r;
 	cout << "a="; cin >> a;
 	cout << "b="; cin >> b;
 	cout << "c="; cin >> c;
 
-	//if the triangle exists
 	if (a < b + c and b < a + c and c < a + b)
 	{
-		//calculate the area
 		P = a + b + c;
 		p = P / 2;
 		S = sqrt(p * (p - a) * (p - b) * (p - c));
-
-		//calculate and output the bisectors
+		l1 = sqrt(a * b * P * (a + b - c)) / (a + b);
+		l2 = sqrt(b * c * P * (b + c - a)) / (b + c);
+		l3 = sqrt(c * a * P * (c + a - b)) / (c + a);
+		R = (a * b * c) / (4 * S);
+		r = (2 * S) / P;
 		cout << "\nThe bisectors:\n";
-		printf("l1=%5.3f\n", sqrt(a * b * P * (a + b - c)) / (a + b));
-		printf("l2=%5.3f\n", sqrt(b * c * P * (b + c - a)) / (b + c));
-		printf("l3=%5.3f\n", sqrt(c * a * P * (c + a - b)) / (c + a));
-
-		//calculate and output the radii
+		printf("l1=%5.3f\n", l1);;
+		printf("l2=%5.3f\n", l2);
+		printf("l3=%5.3f\n", l3);
 		cout << "\nThe radii:\n";
-		printf("R=%5.3f\n", (a * b * c) / (4 * S));
-		printf("r=%5.3f\n", (2 * S) / P);
+		printf("R=%5.3f\n", R);
+		printf("r=%5.3f\n", r);
 	}
-
-	//if the triangle doesn't exist
+	
 	else
 	{
-		cout << "The triangle doesn't exist";
-		return 0;
+		cout << "\nThe triangle doesn't exist\n";
 	}
 }
