69783744a8a8ad0f3d9f1ba2fa52e473a8fc30f4
GoooDiNi <90209078+GoooDiNi@users.noreply.github.com>
2021-11-22 23:16:12 +0200

    Add files via upload

---
+++ OP_Lab4_C++.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+	float x, Sum;
+	int n, i;
+
+	cout << "\ny = 1 / (sin(x)) + 1 / (4*sin(x)) + ... + 1 / (n*n*sin(x))\n--------";
+	cout << "\nDefine x: "; cin >> x;
+	cout << "Define n: "; cin >> n;
+	Sum = 0;
+	for (i = 1; i <= n; i++) 
+	{
+		Sum += sin(x) / (i * i);
+	}
+	cout << "--------\ny = " << Sum << "\n";
+}
