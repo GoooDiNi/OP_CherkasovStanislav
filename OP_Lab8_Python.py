5a6a868012bdf62a5ef589e50e147ce995b3f165
GoooDiNi <90209078+GoooDiNi@users.noreply.github.com>
2021-12-16 21:43:38 +0200

    Update OP_Lab8_Python.py

--- OP_Lab8_Python.py
+++ OP_Lab8_Python.py
@@ -9,7 +9,25 @@ def correct_input(txt, var_type):
             print('Incorrect input!')
             continue
 
+            
+size = correct_input('\nM is an NxN matrix. Define N: ', int)
 
+            
+def matrix_init(m):
+    for i in range(size):
+        m.append([])
+        for j in range(size):
+            m[i].append(rint(-9, 9))
+
+            
+def modify_matrix(m):
+    for i in range(size):
+        for j in range(size):
+            if matrix[i][j] == 0:
+                for k in range(size):
+                    matrix[k][j] = matrix[k][k]
+    
+            
 def print_matrix(m):
     for line in m:
         for numb in line:
@@ -19,21 +37,13 @@ def print_matrix(m):
         print()
 
         
-size = correct_input('\nM is an NxN matrix. Define N: ', int)
-matrix = []
+matrix = []        
+matrix_init(matrix)
 
 print('\nThe original matrix:')
-for i in range(size):
-    matrix.append([])
-    for j in range(size):
-        matrix[i].append(rint(-9, 9))
 print_matrix(matrix)
 
-for i in range(size):
-    for j in range(size):
-        if matrix[i][j] == 0:
-            for k in range(size):
-                matrix[k][j] = matrix[k][k]
+modify_matrix(matrix)
 
 print('\nThe modified matrix:')
 print_matrix(matrix)
